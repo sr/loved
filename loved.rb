5a0edb28114b1b781585a51ce736b44cd639d9c8
Simon Rozet <simon@rozet.name>
2008-10-31 19:09:02 +0100

    clean-up, simplification, various bug fixed

--- loved.rb
+++ loved.rb
@@ -13,127 +13,96 @@ class Hash
   end
 end
 
-class String
-  def to_tags
-    self.inject([]) do |tags, string|
-      string = string.to_s.gsub('.', '').dup
-      string.gsub!(/"(.*?)"\s*\s+?\s*/) { tags << $1; '' }
-      tags.push(*string.split(' '))
-    end
-  end
-end
-
 class MPD
   class Song
     def to_s
-      string = "#{artist} - #{title}"
-      string << "\n   tags: #{tags.join(' ')}" if tags.any?
-      string
+      "#{artist} - #{title}".tap do |string|
+        string << "\n   tags: #{tags.join(' ')}" if tags.any?
+      end
     end
   end
 end
 
-class MPDIsNotPlayingError < StandardError; end
-class AlreadyLovedError < StandardError; end
+module Loved
+  extend self
 
-class Loved
-  class << self
-    def default_configuration
-      { :play_lists_directory => File.join(ENV['HOME'], '.favorites'),
-        :mpd_address   => 'localhost:6600', 
-        :auto_tag_with => %w(artist genre date) }
-    end
+  @@auto_tags = %w(artist genre date)
+  @@directory = File.join(ENV['HOME'], '.loved')
 
-    def default_configuration_file
-      File.join(ENV['HOME'], '.favorites-rc')
-    end
+  def ensure_play_lists_directory_exists!
+    FileUtils.mkdir_p(@@directory) unless File.directory?(@@directory)
+  end
 
-    def playing_song(tags=[], config_file=default_configuration_file)
-      song = new(config_file).love_playing_song(tags)
-      puts "=> Loved #{song.to_s}"
-    rescue AlreadyLovedError
-      abort 'Already loved, fool!'
-    rescue MPDIsNotPlayingError, Errno::ECONNREFUSED
-      abort "MPD ain't playing, fool!"
-    end
+  def mpd
+    MPD.new.tap { |mpd| mpd.connect }
+  rescue SocketError, Errno::ECONNREFUSED
+    abort "Couldn't not connect to MPD"
+  end
 
-    def play(tags, config_file=default_configuration_file)
-      songs = new(config_file).play(tags)
+  def love_current_mpd_song!(tags=[])
+    love_it!(mpd.current_song)
+  end
 
-      puts "Added #{songs.length} songs to your play list. Enjoy!"
+  def append_found_songs_to_mpd_playlist!(tags=[])
+    by_tags(tags).tap do |songs|
+      songs.each { |song| mpd.add(song) }
     end
   end
 
-  def initialize(configuration_file)
-    @configuration_file = configuration_file
-    ensure_play_lists_directory_exists!
-  end
-
-  def love_playing_song(tags=[])
-    raise MPDIsNotPlayingError unless mpd.playing?
-    love_it(mpd.current_song, tags)
-  end
-
-  def play(tags=[])
-    songs = find_by_tags(tags)
-    songs.each { |song| mpd.add(song) }
-
-    songs
-  end
-
-  def love_it(song, tags=[])
+  def love_it!(song, tags=[])
     raise ArgumentError unless song.file
-    raise AlreadyLovedError if loved?(song)
-    auto_tags = song.keep(configuration[:auto_tag_with]).values
-    song.tags = tags.uniq.push(*auto_tags).map! { |tag| %Q{"#{tag}"} }
-    raise AlreadyLovedError if loved?(song)
-    write_to_database(tags) { "#{song.file} # #{song.tags.join(' ')}" }
+
+    return if loved?(song)
+
+    auto_tags = song.keep(@@auto_tags).values
+    song.tags = tags.uniq.push(*auto_tags)
+    p song.tags
+
+    write_to_database(song.tags) { "#{song.file} # #{song.tags.join(' ')}" }
 
     song
   end
 
   def loved?(song, tag='all')
-    find_by_tags(tag.to_a).include?(song.file)
+    by_tags(tag.to_a).include?(song.file)
   end
 
-  def find_all
-    find_songs_in_file('all')
+  def all
+    by_tags(['all'])
   end
 
-  def find_by_tags(tags=[])
+  def by_tags(tags=[])
+    files = tags.map! { |tag| file_name_for_tag(tag) }
+
     case tags.length
-    when 0 then find_all
-    when 1 then find_songs_in_file(file_name_for_tag(tags.first))
+    when 0 then all
+    when 1 then find_songs_in_file(files.first)
     else
-      tags.inject([]) do |songs, tag|
-        songs.push(find_songs_in_file(file_name_for_tag(tags.first)))
+      files.inject([]) do |songs, file|
+        songs << find_songs_in_file(file)
       end
     end
   end
 
-  def configuration
-    @configuration ||= self.class.default_configuration.merge(load_configuration_file)
-  end
-
   private
     def write_to_database(tags, &block)
-      tags.push('all')
-      files = tags.map { |tag| file_name_for_tag(tag) }
+      files = tags.push('all').uniq.map { |tag| file_name_for_tag(tag) }
+
       files.each do |file_name|
         File.open(file_name, 'a') { |f| f.puts "#{yield}\n" }
       end
     end
 
     def find_songs_in_file(file_name)
-      File.foreach(file_name_for_tag('all')).inject([]) do |songs, line|
-        songs.push(line.split('#').first.strip)
+      File.foreach(file_name).inject([]) do |songs, line|
+        songs << line.split('#').first.strip
       end
     rescue Errno::ENOENT
       []
     end
 
     def file_name_for_tag(tag)
-      File.join(configuration[:play_lists_directory], normalize_tag_for_file_name(tag))
+      File.join(@@directory, normalize_tag_for_file_name(tag))
     end
 
     # thanks technoweenie!
@@ -146,29 +115,21 @@ class Loved
       result.downcase!
       result
     end
-
-    def ensure_play_lists_directory_exists!
-      unless File.directory?(configuration[:play_lists_directory])
-        puts "Creating play lists directory"
-        FileUtils.mkdir_p(configuration[:play_lists_directory])
-      end
-    end
-
-    def mpd
-      MPD.new(*configuration[:mpd_address].split(':')).tap { |mpd| mpd.connect }
-    end
-
-    def load_configuration_file
-      YAML.load_file(@configuration_file)
-    rescue Errno::ENOENT
-      {}
-    end
 end
 
 if $0 == __FILE__
-  if ARGV.join =~ /^p\s+/
-    Loved.play(ARGV[1..-1])
+  Loved.ensure_play_lists_directory_exists!
+
+  if ARGV.delete('play')
+    songs = Loved.append_found_songs_to_mpd_playlist!(ARGV)
+    puts "Appended #{songs.length} song(s) to your MPD playlist. Enjoy!"
   else
-    Loved.playing_song(ARGV)
+    song = Loved.love_current_mpd_song!(ARGV.dup)
+    case song
+    when MPD::Song
+      puts "Loved #{song}"
+    else
+      puts 'You really like this song, do you?'
+    end
   end
 end
